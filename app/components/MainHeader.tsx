abcc0dd2188f67100a5828e96e69c1aeac06a4e8
Artem Litvin <mafioznik445@gmail.com>
2024-10-16 16:54:00 +0300

    Fix header background height

--- app/components/MainHeader.tsx
+++ app/components/MainHeader.tsx
@@ -5,7 +5,7 @@ import Logo from "@/assets/logo.png";
 
 const MainHeader = () => {
   return (
-    <header className="max-w-4xl mx-auto px-4 h-16 text-light-yellow flex justify-between items-center gap-6">
+    <header className="_container h-20 text-light-yellow flex justify-between items-center gap-6">
       <Link className="flex gap-5 items-center font-bold uppercase" href="/">
         <Image src={Logo} alt="Logo" width={60} height={60} priority />
         NextLevel Food
--- app/components/MainHeaderBackground.tsx
+++ app/components/MainHeaderBackground.tsx
@@ -3,16 +3,28 @@ import React from "react";
 const MainHeaderBackground = () => {
   return (
     <div className="header-background font-quicksand">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <svg width="1440" height="120" viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_1_3)">
+          <path
+            d="M0 120L48 112.5C96 105 192 90 288 84.9844C384 80.1563 480 84.8437 576 87.5156C672 90 768 90 864 84.9844C960 80.1563 1056 69.8438 1152 62.4844C1248 54.8438 1344 50.1563 1392 47.4844L1440 45V0H1392C1344 0 1248 0 1152 0C1056 0 960 0 864 0C768 0 672 0 576 0C480 0 384 0 288 0C192 0 96 0 48 0H0V120Z"
+            fill="url(#paint0_linear_1_3)"
+          />
+        </g>
         <defs>
-          <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" style={{ stopColor: "#59453c", stopOpacity: "1" }} />
-            <stop offset="100%" style={{ stopColor: "#8f3a09", stopOpacity: "1" }} />
+          <linearGradient
+            id="paint0_linear_1_3"
+            x1="0"
+            y1="0"
+            x2="144000"
+            y2="0"
+            gradientUnits="userSpaceOnUse">
+            <stop stop-color="#59453C" />
+            <stop offset="1" stop-color="#8F3A09" />
           </linearGradient>
+          <clipPath id="clip0_1_3">
+            <rect width="1440" height="120" fill="white" />
+          </clipPath>
         </defs>
-        <path
-          fill="url(#gradient)"
-          d="M0,256L48,240C96,224,192,192,288,181.3C384,171,480,181,576,186.7C672,192,768,192,864,181.3C960,171,1056,149,1152,133.3C1248,117,1344,107,1392,101.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
       </svg>
     </div>
   );
